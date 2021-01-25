cd497e599315d4c225bdcaaeb19950f1fd29402d
Uhiene Gift Eleojo <eleojogiftuhiene@gmail.com>
2021-01-25 20:59:19 +0100

    testing

--- middleware/auth.js
+++ middleware/auth.js
@@ -19,7 +19,7 @@ module.exports = function (req, res, next) {
     return res.status(401).json({ msg: "No token, authorization denied" });
   }
   try {
-    const decoded = jwt.verify(token, config.get("jwtsecret"));
+    const decoded = jwt.verify(token, config.get("jwtSecret"));
 
     
     req.user = decoded.user;
